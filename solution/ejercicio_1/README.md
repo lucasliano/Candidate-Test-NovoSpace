68f5a47154575f2d10e165a048fc0da3b72281bc
Lucas Liaño <lliano@frba.utn.edu.ar>
2021-05-05 18:47:36 -0300

    Testing cocotb - I modify the modul description and also added some test

--- solution/ejercicio_1/README.md
+++ solution/ejercicio_1/README.md
@@ -1,7 +1,7 @@
 # Solución Ejercicio 1
 
 ## Explicación
-Se va a describir un sumador de N bits de entrada. La salida deberá ser de N+1 bits para poder almacenar el resultado de la suma.
+Se va a describir un sumador de N bits de entrada, con reset asincrónico. La salida deberá ser de N+1 bits para poder almacenar el resultado de la suma.
 
 Para esto lo único que debemos tener en consideración es que la llegada de los datos A y B puede no ser simultanea. Esto quiere decir que vamos a tener que esperar a que ambos datos esten disponibles (es decir que si ambas señales de _ready = 0, ya tenemos los dos datos disponibles) para que, en el próximo ciclo de clk, se encuentre en la salida el entero correspondiente a la suma.
 
--- solution/ejercicio_1/main.py
+++ solution/ejercicio_1/main.py
@@ -3,7 +3,7 @@ from nmigen_cocotb import run
 import cocotb
 from cocotb.triggers import RisingEdge, Timer
 from cocotb.clock import Clock
-from random import getrandbits
+from random import randrange
 
 
 
@@ -106,6 +106,7 @@ class Adder(Elaboratable):
         if N < 1:   # If the input argument type is not an integer it should raise a TypeError
             raise InvalidArgument("The argument 'N' should be a natural value greater than 1.")
 
+        self.N = N
 
         # Ports Definition
         self.a = Stream(N, name='a')
@@ -124,42 +125,115 @@ class Adder(Elaboratable):
         # Combinational logic
         # ===================
 
-        comb += self.r.data.eq(signed(self.a.data) + signed(self.a.data))
-        # nMigen generates a signal(N+1) if we need so add two signal(N)
-        # More info: https://nmigen.info/nmigen/latest/lang.html#arithmetic-operators
+        # comb += self.a.ready.eq((~self.r.valid) | (self.r.accepted())) # Si no tengo disponible la salida es porque se reseteo o a.valid o b.valid estan en 0
+        # comb += self.b.ready.eq((~self.r.valid) | (self.r.accepted())) # Cuando no tengo disponible la salida o cuando acepte
 
 
-        # Result
-        with m.If(self.rst):
-            comb += self.r.data.eq(Const(0,signed(N)))              # Async reset
-        with m.Else():
-            sync += self.r.valid.eq(~self.a.ready & ~self.b.ready)  # r_valid <- !a_ready & !b_ready
+
+
+
 
 
         # Sequential logic
         # ===================
-
-        # RX
-        with m.If(self.a.accepted()):           # if a_valid & a_ready
+        with m.If(self.rst):
+            sync += self.r.data.eq(Const(0,signed(self.N)))      # Async reset
+            sync += self.r.valid.eq(1)
             sync += [
-                self.a.ready.eq(0)              # a_data recieved, a_ready <- 0
-            ]
-        with m.If(self.b.accepted()):           # if b_valid & b_ready
-            sync += [
-                self.b.ready.eq(0)              # b_data recieved, b_ready <- 0
-            ]
+                self.a.ready.eq(1),                         # Indicate that i "read" the registers, but the result will be zero
+                self.b.ready.eq(1)
+                ]
+        with m.Else():
+            with m.If(self.a.valid & self.b.valid):         # Wait until both a_data and b_data ara vailable
+                sync += self.r.valid.eq(1)                  # Cuando estan las dos entradas para ser leidas
+                sync += [
+                    self.a.ready.eq(1),                     # Indicate that i'm working with the registers
+                    self.b.ready.eq(1)
+                    ]
+
+                sync += self.r.data.eq(self.a.data + self.b.data)
+                # nMigen generates a signal(N+1) if we need so add two signal(N)
+                # More info: https://nmigen.info/nmigen/latest/lang.html#arithmetic-operators
+            with m.Else():
+                sync += self.r.valid.eq(0)                  # La salida no esta disponible
+                sync += [
+                    self.a.ready.eq(0),                     # Indicate that i'm not able to read cause im waiting both registers
+                    self.b.ready.eq(0)
+                    ]
 
 
-        # TX
-        with m.If(self.r.accepted()):           # if r_valid & r_ready => Data sent! Waiting for incoming a_data & b_data
-            sync += self.r.valid.eq(0)          # r_data was read => r_valid = 0
-            sync += self.a.ready.eq(0)          # r_data was read => a_ready = 1
-            sync += self.b.ready.eq(0)          # r_data was read => b_ready = 1
+
+        # # RX
+        # with m.If(self.a.accepted()):           # if a_valid & a_ready
+        #     sync += [
+        #         self.a.ready.eq(0)              # a_data recieved, a_ready <- 0
+        #     ]
+        #
+        # with m.If(self.b.accepted()):           # if b_valid & b_ready
+        #     sync += [
+        #         self.b.ready.eq(0)              # b_data recieved, b_ready <- 0
+        #     ]
+        #
+        #
+        # # TX
+        # with m.If(self.r.accepted()):           # if r_valid & r_ready => Data sent! Waiting for incoming a_data & b_data
+        #     sync += self.r.valid.eq(0)          # r_data was read => r_valid = 0
+        #     sync += self.a.ready.eq(0)          # r_data was read => a_ready = 1
+        #     sync += self.b.ready.eq(0)          # r_data was read => b_ready = 1
 
         return m
 
+
+# init test inicia el clk, manda un pulso de reset despues de 2 clk.
+async def init_test(dut):
+    cocotb.fork(Clock(dut.clk, 10, 'ns').start())
+    dut.rst <= 1
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    dut.rst <= 0
+
+
+@cocotb.test()
+async def burst(dut):
+    await init_test(dut)
+
+    stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
+    stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
+    stream_output = Stream.Driver(dut.clk, dut, 'r__')
+
+    N = 100
+    width = len(dut.a__data)
+
+    mask = 1 + int('1' * (width-1), 2)  # Retorna valor máximo representable con (width-1) bits.
+
+    data_a = [randrange(-1*mask, mask-1) for _ in range(N)]   # Genera N valores random representables en N bits. (Enteros positivos)
+
+    data_b = [randrange(-1*mask, mask-1) for _ in range(N)]   # Genera N valores random entre -2^(width-1) y 2^(width-1)+1
+
+    expected = [(data_a[i] + data_b[i]) for i in range(N)]    # Calculo la salida esperada como la suma de las entradas
+
+
+    cocotb.fork(stream_input_a.send(data))          # Entrada de datos para el puerto A
+
+    cocotb.fork(stream_input_b.send(data))          # Entrada de datos para el puerto B
+
+    recved = await stream_output.recv(N)            # Guardo los N datos recibidos
+
+    assert recved == expected                       # Consulto si los datos recibidos son iguales a los esperados
+
+
 if __name__ == '__main__':
-    print ("Initializing...")
-    myAdder = Adder(3)
+    print ("Running Tests...")
+    myAdder = Adder(4)
     # myAdder = Adder(-3)
     # myAdder = Adder("3")
+    run(
+        myAdder, 'main',
+        ports=
+        [
+            *list(myAdder.a.fields.values()),
+            *list(myAdder.b.fields.values()),
+            *list(myAdder.r.fields.values())
+        ],
+        vcd_file='adder.vcd'
+    )
