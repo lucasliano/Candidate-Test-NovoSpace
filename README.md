98b80bf8cc50aedc9b49f33c9537d200abfc0745
Lucas Liaño <lliano@frba.utn.edu.ar>
2021-05-07 08:38:48 -0300

    Fixing adder behaivour & Adding Tests - I also modify the planification.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Planificación
 
-### Primera parte (3 días máximo)
+### Primera parte (Actualmente 4 días)
 
 * [x] Leer la consigna del ejercicio 1 detalladamente
 	* [x] Consultar las dudas al respecto.
@@ -11,21 +11,41 @@
 	* [x] cocotb
 	* [x] GTKWave
 	* [x] iverilog
-* [ ] Investigar sobre las herramientas a desarrollar (¿Pára que sirven? ¿Cómo se utilizan? ¿Buenas prácticas?)
+* [x] Investigar sobre las herramientas a desarrollar (¿Pára que sirven? ¿Cómo se utilizan? ¿Buenas prácticas?)
 	* [x] nMigen
-	* [ ] cocotb
+	* [x] cocotb
 * [x] Ejecutar scripts de ejemplo y sacar conclusiones.
 * [x] Replanificar el desarrollo de la solución del primer ejercicio
 * [ ] Solución ejercicio 1:
 	* [x] Plantear el circuito digital correspondiente
 	* [x] Verificar que no haya problemas con la instanciación
-	* [ ] Simular el comportamiento
+	* [x] Simular el comportamiento
 	* [ ] Debuggear!
 	* [ ] Hacer tests!
+		* [x] Reset
+		* [x] Sumas, Restas
+			* [ ] Hacer función toCA2()
+		* [x] Desbordamiento
+		* [x] Burst tests
+		* [ ] Entradas a destiempo
+		* [ ] No r_ready en la salida
+		* [ ] Evaluar el comportamiento para adders con distinta cantidad de bits
+	* [ ] Modularizar el code
 	* [ ] Documentar!
 		* [x] Consultar herramienta y idioma de preferencia!
 		* [x] Comentar el código correspondiente a módulo
 		* [ ] Comentar el código correspondiente a los tests.
+
+
+
+### Segunda parte (2 días máximo)
+* [x] Leer la consigna del ejercicio 1 detalladamente
+	* [ ] Consultar las dudas al respecto.
+* [ ] Ejecutar scripts de ejemplo y sacar conclusiones.
+* [ ] Investigar sobre las herramientas
+	* [ ] Sintaxis de Verilog
+	* [ ] Correcto uso de regular expresions
+* [ ] Replanificar el desarrollo de la solución del primer ejercicio
 
 # Contacto
 
--- solution/ejercicio_1/main.py
+++ solution/ejercicio_1/main.py
@@ -1,7 +1,7 @@
 from nmigen import *
 from nmigen_cocotb import run
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
 from cocotb.clock import Clock
 from random import randrange
 
@@ -113,8 +113,12 @@ class Adder(Elaboratable):
         self.b = Stream(N, name='b')
         self.r = Stream(N+1, name='r')
 
-        # Rst Definition
-        self.rst = Signal()
+        # Reset Values Definition
+        # self.r.data.reset = 0     # Default reset value = 0x0
+        #self.r.valid.reset = 0
+        self.a.ready.reset = 0
+        self.b.ready.reset = 0
+
 
     def elaborate(self, platform):
         # Definitions
@@ -128,63 +132,35 @@ class Adder(Elaboratable):
         # comb += self.a.ready.eq((~self.r.valid) | (self.r.accepted())) # Si no tengo disponible la salida es porque se reseteo o a.valid o b.valid estan en 0
         # comb += self.b.ready.eq((~self.r.valid) | (self.r.accepted())) # Cuando no tengo disponible la salida o cuando acepte
 
-
-
-
-
+        with m.If(self.a.valid & self.b.valid):
+            comb += [
+                self.a.ready.eq(1),                     # Indicate that i'm working with the registers
+                self.b.ready.eq(1)
+                ]
+        with m.Else():
+            comb += [
+                self.a.ready.eq(0),                     # Indicate that i'm not able to read cause im waiting both registers
+                self.b.ready.eq(0)
+                ]
 
 
         # Sequential logic
         # ===================
-        with m.If(self.rst):
-            sync += self.r.data.eq(Const(0,signed(self.N)))      # Async reset
-            sync += self.r.valid.eq(1)
-            sync += [
-                self.a.ready.eq(1),                         # Indicate that i "read" the registers, but the result will be zero
-                self.b.ready.eq(1)
-                ]
+        with m.If(self.a.valid & self.b.valid):         # Wait until both a_data and b_data ara vailable
+            sync += self.r.valid.eq(1)                  # Cuando estan las dos entradas para ser leidas
+
+            sync += self.r.data.eq(self.a.data.as_signed() + self.b.data.as_signed())
+            # nMigen generates a signal(N+1FallingEdge) if we need so add two signal(N)
+            # More info: https://nmigen.info/nmigen/latest/lang.html#arithmetic-operators
         with m.Else():
-            with m.If(self.a.valid & self.b.valid):         # Wait until both a_data and b_data ara vailable
-                sync += self.r.valid.eq(1)                  # Cuando estan las dos entradas para ser leidas
-                sync += [
-                    self.a.ready.eq(1),                     # Indicate that i'm working with the registers
-                    self.b.ready.eq(1)
-                    ]
+            with m.If(self.r.accepted()):               # If r_ready & r_valid
+                sync += self.r.valid.eq(0)              # The output was read and it is not longer available.
 
-                sync += self.r.data.eq(self.a.data + self.b.data)
-                # nMigen generates a signal(N+1) if we need so add two signal(N)
-                # More info: https://nmigen.info/nmigen/latest/lang.html#arithmetic-operators
-            with m.Else():
-                sync += self.r.valid.eq(0)                  # La salida no esta disponible
-                sync += [
-                    self.a.ready.eq(0),                     # Indicate that i'm not able to read cause im waiting both registers
-                    self.b.ready.eq(0)
-                    ]
-
-
-
-        # # RX
-        # with m.If(self.a.accepted()):           # if a_valid & a_ready
-        #     sync += [
-        #         self.a.ready.eq(0)              # a_data recieved, a_ready <- 0
-        #     ]
-        #
-        # with m.If(self.b.accepted()):           # if b_valid & b_ready
-        #     sync += [
-        #         self.b.ready.eq(0)              # b_data recieved, b_ready <- 0
-        #     ]
-        #
-        #
-        # # TX
-        # with m.If(self.r.accepted()):           # if r_valid & r_ready => Data sent! Waiting for incoming a_data & b_data
-        #     sync += self.r.valid.eq(0)          # r_data was read => r_valid = 0
-        #     sync += self.a.ready.eq(0)          # r_data was read => a_ready = 1
-        #     sync += self.b.ready.eq(0)          # r_data was read => b_ready = 1
 
         return m
 
 
-# init test inicia el clk, manda un pulso de reset despues de 2 clk.
+
 async def init_test(dut):
     cocotb.fork(Clock(dut.clk, 10, 'ns').start())
     dut.rst <= 1
@@ -193,33 +169,164 @@ async def init_test(dut):
     dut.rst <= 0
 
 
+def toCA2(arg, N):      # TODO
+    '''
+    Function Description
+    ------------------
+    Converts an int to the N-bit 2's complement representation
+
+    Parameters
+    ----------
+    arg : int
+        Target number.
+
+    N : int
+        Number of bits of arg.
+
+    '''
+    raise RuntimeError("This function is not finished")
+    return arg
+
+
+@cocotb.test()
+async def reset_test(dut):
+    '''
+    Test Description
+    ------------------
+    Checks if the values after the reset are correct.
+
+    '''
+    # Definitions
+    stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
+    stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
+    stream_output = Stream.Driver(dut.clk, dut, 'r__')
+
+    width = len(dut.a__data)
+
+    # Test Data
+    data_a = [1, 2, 3]
+    data_b = [4, 5, 6]
+    expected = [0, 0, 0, 0]
+    recved =   [0, 0, 0, 0]     # Initial recieved values
+
+    # Test Execution
+    cocotb.fork(Clock(dut.clk, 10, 'ns').start())
+    await RisingEdge(dut.clk)
+    dut.rst <= 1
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    dut.rst <= 0
+    cocotb.fork(stream_input_a.send(data_a))
+    cocotb.fork(stream_input_b.send(data_b))
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    dut.rst <= 1
+    await RisingEdge(dut.clk)
+    dut.rst <= 0
+
+    await FallingEdge(dut.clk)                          # Reads when FallingEdge(clk)
+    recved[0] = stream_output.data.value.integer
+    recved[1] = stream_output.valid.value.integer
+    recved[2] = stream_input_a.ready.value.integer
+    recved[3] = stream_input_b.ready.value.integer
+    await RisingEdge(dut.clk)
+
+    assert recved == expected
+
+@cocotb.test()
+async def basic_add_subs(dut):
+    '''
+    Test Description
+    ------------------
+    Checks if the adder is working. We check all the posible combinations (+)(+), (+)(-), (-)(+) and (-)(-).
+    '''
+    # Definitions
+    stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
+    stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
+    stream_output = Stream.Driver(dut.clk, dut, 'r__')
+
+    width = len(dut.a__data)
+
+    # Test Data
+    data_a =   [3, -2,  3, -2]
+    data_b =   [2,  3, -4, -2]
+    expected = [5,  1, -1, -4]                 # The expected result is the sum of the data_a + data_b values
+
+    # Test Execution
+    await init_test(dut)
+    cocotb.fork(stream_input_a.send(data_a))        # Port A input
+    cocotb.fork(stream_input_b.send(data_b))        # Port B input
+    recved = await stream_output.recv(len(data_a))  # Save the N values recieved
+
+
+    recved_processed = [toCA2(recved[_],width) for _ in range(len(recved))]         # Convert the int to a string containing the N-bit ca2 binary equivalent
+    expected_processed = [toCA2(expected[_],width) for _ in range(len(expected))]
+
+    assert recved_processed == expected_processed
+
+@cocotb.test()
+async def overflow(dut):
+    '''
+    Test Description
+    ------------------
+    Checks what happen if we want to add two numbers that are thought to overflow.
+    '''
+    # Definitions
+    stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
+    stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
+    stream_output = Stream.Driver(dut.clk, dut, 'r__')
+
+    width = len(dut.a__data)
+    mask = 1 + int('1' * (width-1), 2)  # max value with (width-1) bits = (2^(width-1))
+
+    # Test Data
+    data_a =   [mask - 1  , -1*mask]
+    data_b =   [mask - 1  , -1*mask]
+    expected = [2*mask - 2, -2*mask]         # This should cause an overflow exception in width-1 bits.
+
+    # Test Execution
+    await init_test(dut)
+    cocotb.fork(stream_input_a.send(data_a))        # Port A input
+    cocotb.fork(stream_input_b.send(data_b))        # Port B input
+    recved = await stream_output.recv(len(data_a))  # Save the N values recieved
+
+    recved_processed = [toCA2(recved[_],width) for _ in range(len(recved))]         # Convert the int to a string containing the N-bit ca2 binary equivalent
+    expected_processed = [toCA2(expected[_],width) for _ in range(len(expected))]
+
+    assert recved_processed == expected_processed
+
 @cocotb.test()
 async def burst(dut):
-    await init_test(dut)
-
+    '''
+    Test Description
+    ------------------
+    Stress test with random numbers.
+    '''
+    # Definitions
     stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
     stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
     stream_output = Stream.Driver(dut.clk, dut, 'r__')
 
     N = 100
     width = len(dut.a__data)
+    mask = 1 + int('1' * (width-1), 2)  # max value with (width-1) bits = (2^(width-1))
 
-    mask = 1 + int('1' * (width-1), 2)  # Retorna valor máximo representable con (width-1) bits.
+    # Test Data
+    data_a = [randrange(-1*mask, mask-1) for _ in range(N)]   # This generates random numbres between -2^(width-1) and 2^(width-1)+1
+    data_b = [randrange(-1*mask, mask-1) for _ in range(N)]
+    expected = [(data_a[_] + data_b[_]) for _ in range(N)]    # Computes the expected result
 
-    data_a = [randrange(-1*mask, mask-1) for _ in range(N)]   # Genera N valores random representables en N bits. (Enteros positivos)
+    # Test Execution
+    await init_test(dut)
+    cocotb.fork(stream_input_a.send(data_a))        # Port A input
+    cocotb.fork(stream_input_b.send(data_b))        # Port B input
+    recved = await stream_output.recv(N)            # Save the N values recieved
 
-    data_b = [randrange(-1*mask, mask-1) for _ in range(N)]   # Genera N valores random entre -2^(width-1) y 2^(width-1)+1
+    recved_processed = [toCA2(recved[_],width) for _ in range(len(recved))]         # Convert the int to a string containing the N-bit ca2 binary equivalent
+    expected_processed = [toCA2(expected[_],width) for _ in range(len(expected))]
 
-    expected = [(data_a[i] + data_b[i]) for i in range(N)]    # Calculo la salida esperada como la suma de las entradas
+    assert recved_processed == expected_processed
 
-
-    cocotb.fork(stream_input_a.send(data))          # Entrada de datos para el puerto A
-
-    cocotb.fork(stream_input_b.send(data))          # Entrada de datos para el puerto B
-
-    recved = await stream_output.recv(N)            # Guardo los N datos recibidos
-
-    assert recved == expected                       # Consulto si los datos recibidos son iguales a los esperados
 
 
 if __name__ == '__main__':
